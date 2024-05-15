50dade8d413824887c23017808b267f034b7afbc
Wellington A.S. Aguiar <wellingtonasaguiar@gmail.com>
2024-05-15 12:41:54 -0300

    Aula11_POO_Heranca

---
+++ heranca/src/Mensalista.java
@@ -0,0 +1,26 @@
+public class Mensalista extends Empregado{
+    private String cargo;
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo){
+        this.cargo = cargo;
+    }
+
+    public Mensalista(String nome, String endereco, double salario, String cargo){
+        super(nome,endereco , salario);
+        this.cargo = cargo;
+    }
+
+    public void calcularSalario(){
+        if(cargo.equals("Junior")){
+            salario = 2500;
+        }else if (cargo.equals("Pleno")){
+            salario = 3500;
+        }else {
+            salario =5500;
+        }
+    }
+}
